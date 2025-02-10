5aa1cd771413f86258fba87497f4644118e6b257
leomcg <leomcg@gmail.com>
2025-02-10 18:24:36 -0300

    adicionando rotas para buscar usuarios e posts por id

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/", postsRoutes);
+app.use("/api/posts", postsRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({
+    message: error.message || "Ops... algo deu errado, tente novamente.",
+  });
+});
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
--- backend/routes/posts-routes.js
+++ backend/routes/posts-routes.js
@@ -1,9 +1,57 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("get request for posts");
-  res.json({ message: "get request for posts" });
+const DUMMY_POSTS = [
+  {
+    id: "p1",
+    title: "My First Post",
+    description: "This is the description of my first post.",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "My Second Post",
+    description: "This is my second post.",
+    creator: "u1",
+  },
+  {
+    id: "p3",
+    title: "A Day in the Life",
+    description: "Sharing my daily routine and experiences.",
+    creator: "u2",
+  },
+  {
+    id: "p4",
+    title: "Travel Diaries",
+    description: "Documenting my travels around the world.",
+    creator: "u3",
+  },
+];
+
+router.get("/:pid", (req, res) => {
+  const postId = req.params.pid;
+  const post = DUMMY_POSTS.find((post) => {
+    return post.id === postId;
+  });
+
+  if (!post) {
+    return res.status(404).json({ message: "Post não encontrado" });
+  }
+
+  res.json({ post });
+});
+
+router.get("/user/:uid", (req, res) => {
+  const userId = req.params.uid;
+  const posts = DUMMY_POSTS.filter((post) => {
+    return post.creator === userId;
+  });
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).json({ message: "Usuário não encontrado" });
+  }
+
+  res.json({ posts });
 });
 
 module.exports = router;
