9040978cf6f770d993b3102346f29936b6e34080
leomcg <leomcg@gmail.com>
2025-02-17 10:49:47 -0300

    muda minimo da senha para 5 no back

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -11,7 +11,7 @@ router.post(
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
+    check("password").isLength({ min: 5 }),
   ],
   signup
 );
@@ -20,7 +20,7 @@ router.post(
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
+    check("password").isLength({ min: 5 }),
   ],
   login
 );
